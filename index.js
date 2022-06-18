1bc538598e710d0fe31cf0965396c81c37144d71
Covasc <lucas.rodmarques@gmail.com>
2022-06-18 12:30:07 -0300

    post sign-up

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-import express from 'express';
+import express, { request } from 'express';
 
 const server = express();
 server.use(express.json());
 
 const userList = [];
 const tweets = [];
+
+server.post('/sign-up', (request, response) => {
+    const user = request.body;
+    userList.push(user);
+    console.log('OK');
+})
 
 server.listen(6000);
